2b9edd9df6075ef102a750fabf53c6eb71f918ec
isa349 <isaretweet@gmail.com>
2024-10-04 10:54:54 -0600

    first commit

--- Server.py
+++ Server.py
@@ -2,16 +2,17 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+#@app.route('/')
+#def index():
+#    return 'Hello, World!'
+
+#@app.get('/')
+#def html():
+
+#    return render_template('server.html')
+
 @app.route('/')
-def index():
-    return 'Hello, World!'
-
-@app.get('/htm')
 def html():
-    return render_template('server.html')
-
-@app.get('/dato')
-def example():
     return render_template('curriculum.html')
 
 if __name__ == '__main__':
